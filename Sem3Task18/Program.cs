ca674b17f72d47e304c8490f7f117912c532892f
Umnokisa <kudahtalka@gmail.com>
2023-01-31 13:49:30 +0300

    Sem3 19-22

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача №18
+// Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int qNum = ReadData("Введите номер четверти");
+PrintQuterAnswer(qNum);
+
+//Метод читает данные от пользователя
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+// Метод определяет допустимого диапазона координат точек по четверти
+void PrintQuterAnswer(int num)
+{
+    if (num > 0 && num < 5)
+    {
+        if (num == 1) Console.WriteLine("X > 0, Y > 0");
+        if (num == 2) Console.WriteLine("X > 0, Y < 0");
+        if (num == 3) Console.WriteLine("X < 0, Y < 0");
+        if (num == 4) Console.WriteLine("X < 0, Y > 0");
+    }
+    else Console.WriteLine("Вы ввели не номер четверти!");
+}
