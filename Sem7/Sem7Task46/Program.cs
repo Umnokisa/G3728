1c0c79ea50879907f09b6b026b0767b45fd08bc7
Umnokisa <kudahtalka@gmail.com>
2023-02-14 14:57:54 +0300

    Sem7 Task 46, 48, 49, 51

--- Sem7/Sem7Task46/Program.cs
+++ Sem7/Sem7Task46/Program.cs
@@ -2,71 +2,23 @@
 // // Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
 
 
-// ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
-//                                         ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
-//                                         ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
-//                                         ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
-//                                         ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
-//                                         ConsoleColor.Yellow};
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
 
-// //1) Получение данных от пользователя
-// int n = InputNum("Количество строк матрицы");
-// int m = InputNum("Количество строк матрицы");
+//1) Получение данных от пользователя
+int n = InputNum("Количество строк матрицы");
+int m = InputNum("Количество строк матрицы");
 
-// //2) Генерация двумерного массива
-// int[,] matix = FillTwoDimArray(n,m);
+//2) Генерация двумерного массива
+int[,] matix = FillTwoDimArray(n,m);
 
-// //3) Печатаем двумерный массив
-// PrintTwoDimArray(matix);
+//3) Печатаем двумерный массив
+PrintTwoDimArray(matix);
 
-
-// int InputNum(string msg)
-// {
-//     Console.Write(msg);
-//     return int.Parse(Console.ReadLine() ?? "0");
-// }
-
-// // метод для заполнения двумерного массива
-// int[,] FillTwoDimArray(int countRow, int countColumn)
-// {
-//     System.Random numberSyntezator = new System.Random();
-//     int i = 0; int j = 0;
-//     int[,] outArray = new int[countRow, countColumn];
-//     while (i < countRow)
-//     {
-//         j = 0;
-//         while (j < countColumn)
-//         {
-//             outArray[i, j] = numberSyntezator.Next(0, 101);
-//             j++;
-//         }
-//         i++;
-//     }
-//     return outArray;
-// }
-
-
-// //метод для печати двумерного массива
-// void PrintTwoDimArray(int[,] inputArray)
-// {
-//     int i = 0; int j = 0;
-
-//     while (i < inputArray.GetLength(0))
-//     {
-//         j = 0;
-//         while (j < inputArray.GetLength(1))
-//         {
-//             Console.ForegroundColor = (ConsoleColor)(new System.Random().Next(0,16));
-//             //col[new System.Random().Next(0,16)]; 
-//             Console.Write(inputArray[i, j] + " ");
-//             Console.ResetColor(); 
-//             j++;
-//         }
-//         //Console.Write("\n");
-//         Console.WriteLine();
-//         i++;
-//     }
-// }
 
 int InputNum(string msg)
 {
@@ -74,32 +26,44 @@ int InputNum(string msg)
     return int.Parse(Console.ReadLine() ?? "0");
 }
 
-int[,] CreateArray(int xlen, int ylen)
+// метод для заполнения двумерного массива
+int[,] FillTwoDimArray(int countRow, int countColumn)
 {
-    int[,] arr = new int[ylen, xlen];
-    for (int i = 0; i < ylen; i++)
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
     {
-        for (int j = 0; j < xlen; j++)
+        j = 0;
+        while (j < countColumn)
         {
-            arr[i, j] = j+i;
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
         }
+        i++;
     }
-    return arr;
+    return outArray;
 }
 
-void ShowArray(int[,] arr)
+
+//метод для печати двумерного массива
+void PrintTwoDimArray(int[,] inputArray)
 {
-    for (int i = 0; i <= arr.GetUpperBound(0); i++)
+    int i = 0; int j = 0;
+
+    while (i < inputArray.GetLength(0))
     {
-        for (int j = 0; j <= arr.GetUpperBound(1); j++)
+        j = 0;
+        while (j < inputArray.GetLength(1))
         {
-            Console.Write(arr[i, j] + "\t");
+            Console.ForegroundColor = (ConsoleColor)(new System.Random().Next(0,16));
+            //col[new System.Random().Next(0,16)]; 
+            Console.Write(inputArray[i, j] + " ");
+            Console.ResetColor(); 
+            j++;
         }
-        Console.WriteLine("");
+        //Console.Write("\n");
+        Console.WriteLine();
+        i++;
     }
 }
-
-int xlen = InputNum("Ваше число столбцов:");
-int ylen = InputNum("Ваше число строк:");
-int[,] matrix = CreateArray(xlen, ylen);
-ShowArray(matrix);
--- Sem7/Sem7Task51/Program.cs
+++ Sem7/Sem7Task51/Program.cs
@@ -1,6 +1,6 @@
-﻿// Задача №48
-// Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: 
-// Aₘₙ = m+n. Выведите полученный массив на экран.
+﻿// Задача №51
+// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0); (1;1) и т.д.
 
 int ReadData(string line)
 {
@@ -10,7 +10,7 @@ int ReadData(string line)
 }
 
 // Универсальный метод генерации и заполнение двумерного массива
-int[,] Fill2DIndexed(int countRow, int countColumn)
+int[,] Fill2DArray(int countRow, int countColumn, int topBorder, int downBorder)
 {
     System.Random rand = new System.Random();
     int[,] array2D = new int[countRow, countColumn];
@@ -19,35 +19,54 @@ int[,] Fill2DIndexed(int countRow, int countColumn)
     {
         for (int j = 0; j < countColumn; j++)
         {
-            array2D[i, j] = i + j;
+            array2D[i, j] = rand.Next(topBorder, downBorder + 1);
         }
     }
     return array2D;
 }
- 
-ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+
+int CalcDiag(int[,] matrix)
+{
+    int res = 0;
+    int min = matrix.GetLength(0) < matrix.GetLength(1) ? matrix.GetLength(0) : matrix.GetLength(1);
+
+    for (int i = 0; i < min; i++)
+    {
+        res += matrix[i, i];
+    }
+
+    return res;
+}
+
+void Print2DArrayColor(int[,] matrix)
+{
+    ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
                                         ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
                                         ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
                                         ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
                                         ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
                                         ConsoleColor.Yellow};
-
-void Print2DArrayColor(int[,] matrix)
-{
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
             Console.ForegroundColor = col[new System.Random().Next(0, 16)];
-            Console.Write(matrix[i, j]+"_____".Substring(matrix[i, j].ToString().Length));
+            Console.Write(matrix[i, j] + "     ".Substring(matrix[i, j].ToString().Length));
             Console.ResetColor();
         }
         Console.WriteLine();
     }
 }
 
+void PrintData(string msg, int result)
+{
+    Console.WriteLine(msg + result);
+}
+
 
 int row = ReadData("Введите количество строк ");
 int column = ReadData("Введите количество столбцов ");
-int[,] arr2D = Fill2DIndexed(row, column);
+int[,] arr2D = Fill2DArray(row, column, 10, 99);
 Print2DArrayColor(arr2D);
+int sumOfDiag = CalcDiag(arr2D);
+PrintData("Сумма элементов на главной диагонали равна: ", sumOfDiag);
